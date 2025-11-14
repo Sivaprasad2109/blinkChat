02cc99605f889cea3c82eef90df7bcce57eb6356
Sivaprasad2109 <sivamaha2109@gmail.com>
2025-11-14 13:26:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ setInterval(() => {
 // =====================
 // 🚀 Start Server
 // =====================
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Running on ${PORT}`));
+
 
 
